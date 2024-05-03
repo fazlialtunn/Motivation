c29ace1d3991058bca4719b2511b2538036acdae
Fazlı Altun <32793348+fazlialtunn@users.noreply.github.com>
2024-05-03 12:11:31 +0300

    Initial Commit

---
+++ Motivation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Motivation
+//
+//  Created by Fazli Altun on 3.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
